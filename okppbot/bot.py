88c70d4719d1394e8b5b2d983c06e390adb0ba67
tolfino <39185647+tolfino@users.noreply.github.com>
2021-01-17 21:24:23 -0800

    ok pp bot

---
+++ okppbot/bot.py
@@ -0,0 +1,37 @@
+import discord
+import os
+import re
+
+bot = discord.Client()
+
+from .words import WORDS, sanitize
+
+
+def should_delete(content):
+    content = sanitize(content)
+    for word in re.split(r'\s+', content):
+        if word.isdigit():
+            continue
+        if word not in WORDS:
+            return True
+    return False
+
+
+async def process_message(channel_id, message_id, content):
+    if not should_delete(content):
+        return
+    await bot.http.delete_message(channel_id, message_id, reason='ok pp head')
+
+
+@bot.event
+async def on_message(message):
+    await process_message(message.channel.id, message.id, message.content)
+
+
+@bot.event
+async def on_raw_message_edit(payload):
+    await process_message(payload.channel_id, payload.message_id, payload.data['content'])
+
+
+def main(token=os.getenv('OKPPBOT_TOKEN')):
+    bot.run(token)
---
+++ okppbot/words.py
@@ -0,0 +1,9 @@
+import os
+import re
+
+
+def sanitize(s):
+    return re.sub(r'[^a-z0-9 ]', '', s.lower())
+
+
+WORDS = {sanitize(l) for l in open(os.path.join(os.path.dirname(__file__), 'words.txt'))}
